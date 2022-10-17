139f15cf039c097ef0d0db0b63aab8542e2db0c7
AshJF <aj16ferns@gmail.com>
2022-10-17 16:05:09 +0200

    First commit

---
+++ Readme.txt
@@ -0,0 +1,14 @@
+DATA ENRICHMENT UTILITY
+-----------------------
+
+The "main.py" contains the Python code for the desired enrichment of the data.
+"Requirements.txt" has been included and it contains the names of the required Python libraries.
+"Data.csv" contains the dummy data that we use for this operation.
+"run.bat" contains the required commands to run the script automatically.
+
+To run the script :
+1) Make sure to have Python installed in your machine.
+2) Extract or Clone all the files to a folder in your machine.
+2) Click on the "run.bat" with necessary permissions.
+3) Close the resulting command prompt window once all commands are executed.
+4) The file "Output.csv" can be found in the same folder as all the files.
---
+++ main.py
@@ -0,0 +1,69 @@
+import requests
+import pandas as pd
+
+def run_enrichment(data, lei_data):
+    """
+    Function to carry out all necessary enrichment steps.
+
+    Args:
+        data (DataFrame row)
+        lei_data (json)
+
+    Returns:
+        DataFrame row
+    """
+
+    lei_info = [x for x in lei_data if x["attributes"]["lei"] == data["lei"]][0]
+    data["name"] = lei_info["attributes"]["entity"]["legalName"]["name"]
+    data["bic"] = ", ".join(lei_info["attributes"]["bic"])
+    country = lei_info["attributes"]["entity"]["legalAddress"]["country"]
+    
+    if country == "GB":
+        data["transaction_costs"] =  round((data["notional"] * data["rate"]) - data["notional"], 2)
+    elif country == "NL":
+        data["transaction_costs"] = round(abs((data["notional"] * (1/data["rate"])) - data["notional"]), 2)
+        
+    return data
+
+def get_lei_info(lei_list):
+    """
+    Function to fetch data from the external API.
+
+    Args:
+        lei_list (list): Unique lei values from DataFrame
+
+    Returns:
+        json
+    """
+    
+    api = "https://api.gleif.org/api/v1/lei-records"
+    param_string = ", ".join(lei_list)
+    query_params = {"filter[lei]": f"{param_string}"}
+    
+    response = requests.get(api, params=query_params).json()
+
+    return response
+
+def main(file_name):
+    """
+    Main Function.
+
+    Args:
+        file_name (string)
+    """
+    
+    print("Reading CSV data.")
+    data = pd.read_csv(file_name)
+
+    print("Reading lei data from external API.")
+    response = get_lei_info(data['lei'].unique())
+
+    print("Begin Enrichment.")
+    data = data.apply(run_enrichment, args=[response["data"]], axis=1)
+    print("Enrichment Completed.")
+
+    data.to_csv("Output.csv", index=False)
+    print("Output File Created.")
+
+if __name__ == "__main__":
+    main("Data.csv")
